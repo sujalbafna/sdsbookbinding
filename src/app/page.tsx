678b1bb197718722a136dcb25c32a7bb78567bd9
Sujal Bafna <bafna3249@gmail.com>
2026-01-08 09:49:36 +0000

    remove sectin "Instant Price Estimator, Customize Your Book, Upload Your

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Hero } from '@/components/sections/hero';
 import { Services } from '@/components/sections/services';
-import { PriceEstimator } from '@/components/sections/price-estimator';
 import { OrderTracking } from '@/components/sections/order-tracking';
 import { Contact } from '@/components/sections/contact';
 
@@ -13,8 +12,8 @@ export default function Home() {
       <main className="flex-1">
         <Hero />
         <Services />
-        <PriceEstimator />
         <OrderTracking />
+        <Contact />
       </main>
       <Footer />
     </div>
